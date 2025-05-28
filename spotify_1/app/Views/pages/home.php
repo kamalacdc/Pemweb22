18611b845e7f4844167a17af6c048baf7d0de2ec
kamalacdc <ahmaddiankamal@saintek.unipdu.ac.id>
2025-05-28 10:02:12 +0700

    update

--- spotify_1/app/Views/pages/home.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layout/main_layout') ?>
-
-<?= $this->section('content') ?>
-<h1>Selamat Datang di MusikApp</h1>
-<p>Nikmati musik favorit Anda!</p>
-<?= $this->endSection() ?>
--- spotify_1/app/Views/pages/playlist.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend('layout/main_layout') ?>
-
-<?= $this->section('content') ?>
-<h1>Playlist Favorit</h1>
-<ul>
-    <li>📀 Lagu Favorit 1</li>
-    <li>📀 Lagu Favorit 2</li>
-</ul>
-<?= $this->endSection() ?>
